e167e1923cbc9a2cca545668807e5c7b7c1d2761
TadasPaplauskas <tadaspaplauskas@gmail.com>
2016-03-26 17:08:41 +0200

    fixed cancelled social login issue when exception would not be handled

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -136,9 +136,16 @@ class AuthController extends Controller
 
     public function handleProviderCallbackFacebook()
     {
-        $user = Socialite::driver('facebook')->user();
+        try
+        {
+            $user = Socialite::driver('facebook')->user();
 
-        return $this->socialLogin($user);
+            return $this->socialLogin($user);
+        }
+        catch (\Exception $e)
+        {
+            return redirect()->route('login');
+        }
     }
 
     public function redirectToProviderGoogle()
@@ -148,11 +155,17 @@ class AuthController extends Controller
 
     public function handleProviderCallbackGoogle()
     {
-        $user = Socialite::driver('google')->user();
+        try
+        {
+            $user = Socialite::driver('google')->user();
 
-        return $this->socialLogin($user);
+            return $this->socialLogin($user);
+        }
+        catch (\Exception $e)
+        {
+            return redirect()->route('login');
+        }
     }
-
 
     public function socialLogin($user)
     {
